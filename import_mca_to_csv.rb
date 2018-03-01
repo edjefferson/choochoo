e083e35e6ad8641a2bde2840384a13de3b70a607
Ed Jefferson <e.jefferson@gmail.com>
2018-03-01 19:17:39 +0000

    add non-rails import option b/c it's faster

---
+++ import_mca_to_csv.rb
@@ -0,0 +1,40 @@
+require 'active_record'
+require './app/lib/import.rb'
+require 'csv'
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => "postgresql",
+  :host     => "localhost",
+  :database => "ChooChoo_development"
+)
+
+class TiplocInsert < ActiveRecord::Base
+end
+
+class BasicSchedule < ActiveRecord::Base
+end
+
+class BasicScheduleExtraDetail < ActiveRecord::Base
+end
+
+class OriginStation < ActiveRecord::Base
+end
+
+class IntermediateStation < ActiveRecord::Base
+end
+
+class TerminatingStation < ActiveRecord::Base
+end
+
+class Association < ActiveRecord::Base
+end
+
+class ChangeEnRoute < ActiveRecord::Base
+end
+
+def run_import_sequence(filename)
+  import = Import.new(filename)
+  import.start_import
+end
+
+ run_import_sequence("ttis828/ttisf828.mca")
